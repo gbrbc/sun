d6506199c76c6e6d84d5a7eb1d895e782a606a92
gbrbc <brendanreillybc@gmail.com>
2025-07-20 14:11:39 -0400

    new wall2polygon2

--- testwall.py
+++ testwall.py
@@ -1,4 +1,5 @@
-import warnings
+#import warnings
+#warnings.filterwarnings("error", category=UserWarning)
 import sys
 import os
 from shapely.geometry import Polygon, MultiPolygon, Point, LineString
@@ -13,7 +14,7 @@ import pandas as pd
 
 #import geographiclib
 
-from rotateline2 import *
+from rotateline4 import *
 
 from to_coords import *
 
@@ -22,18 +23,12 @@ from geographiclib.geodesic import Geodesic
 from makerec3 import *
 
 from getpos import *
-
-#warnings.filterwarnings("error", category=UserWarning)
-
+from geotools import * 
 
 
 
 
 
-def Deb(msg=""):
-    print(f"Debug {sys._getframe().f_back.f_lineno}: {msg}", flush=True,file=sys.stderr)
-    sys.stdout.flush()
-    sys.stderr.flush()
 
 
 
@@ -48,179 +43,250 @@ def swappoint(apoint):
 
 
 
-def wall2polygon(wall_list):
+def wall2polygon2(alist):
+    """!
+@callergraph
+
+
+
+@callgraph
+    """
+
+
     global total
 
     wallnum=0
+    
+    for wall1 in alist:
+        for wall2 in wall1:
+            Deb(wall2)
 
-    havehalf=1                  # have only 1 of 2 tuples
+            b=wall2
 
-    for wall1,wall2 in wall_list:
-
-
-        if havehalf:
-            b=[(wall1,wall2)]
-            inv1=wall2
-            inv2=wall1
-            havehalf=0
-            continue
-
-        assert         havehalf==0
-
-        havehalf=1
-        inv3=wall2
-        inv4=wall1
-
-
-        b=[b[0],(wall1,wall2)]
-
-        wallnum=wallnum+1
-
-    #    a=Geodesic.WGS84.Inverse( 40.756579563884,-73.971558876777,  40.756537332775,-73.971583976616)
-        Deb(type(wall1))
-        Deb(wall1)
-
-        a=Geodesic.WGS84.Inverse(inv1,inv2,inv3,inv4)
-
-
-        Deb(a)
-
-        print("Initial line Length ",a['s12'],"   azimuth ",180.0+a['azi1'])
-
-
-
+            wallnum=wallnum+1
 
         ############################################
-
-    #    b= [(-73.971558876777, 40.756579563884), (-73.971583976616, 40.756537332775)]
-
-#        b = [(wall1,wall2),(wall3,wall4)]
-
-        k=1
+            k=1
 
 
-        sunaz=sunposition['azimuth']  #149.617534
-        assert (0.0+sunaz)<=360
+            sunaz=sunposition['azimuth']  #149.617534
+            assert (0.0+sunaz)<=360
 
-        elevation=sunposition['altitude']  #13.197431
-        height=45
+            elevation=1.0*sunposition['altitude']  #13.197431
+            height=45
 
-        dshit2 = gpd.GeoDataFrame(geometry=[LineString(b)], crs="WGS84")
-
-
-
-        dshit3=dshit2.to_crs("EPSG:3857")
-
-        Deb(type(dshit2))
-        Deb(type(dshit3))
+            dshit2 = gpd.GeoDataFrame(geometry=[LineString(b)], crs="WGS84")
 
 
-        dshit3['centroid']=dshit3['geometry'].centroid
+            barney = dshit2.to_json(to_wgs84=True)  # =True)  #  crs="EPSG:3627"
+
+            with open("/tmp/prerot" + str(wallnum) + ".json", "w") as w1:
+                w1.write(barney)
+                w1.close()
+
+            dshit3=dshit2.to_crs("EPSG:3857")
+
+            Deb(type(dshit2))
+            Deb(type(dshit3))
 
 
-
-        assert (0.0+a['azi1'])<=360
-
-##skip rotate
-        skiprotate=False
-        if skiprotate:
-            dshit4=dshit2
-            dshit=dshit2
-        else:
-    ## Previously added 180 to line's az
-            Deb("Rotate "+str(180.0+a['azi1'])+"  to  "+str(sunaz))
-            dshit = rotateline2(LineString(b),180.0+a['azi1'], sunaz)
-            #dshit = rotateline(dshit3.geometry.get_coordinates(),180.0+a['azi1'], sunaz)
-
-            dshit4 = gpd.GeoDataFrame(geometry=[dshit], crs="WGS84")
-            dshit2=dshit4
-            dshit=dshit4
+            dshit3['centroid']=dshit3['geometry'].centroid
 
 
 
-        barney = dshit4.to_json(to_wgs84=True)  # =True)  #  crs="EPSG:3627"
+    #17        assert (0.0+a['azi1'])<=360
 
-        with open("/tmp/fromwall" + str(wallnum) + ".json", "w") as w1:
-            w1.write(barney)
-            w1.close()
+    ##skip rotate
+            skiprotate=False
+            if skiprotate:
+                dshit4=dshit2
+                dshit=dshit2
+            else:
+        ## Previously added 180 to line's az
+                wallaz= calculate_azimuth_line(LineString(b))
+                Deb("Rotate "+str(wallaz)+"  to  "+str(sunaz))
+                dshitline = rotateline4(LineString(b), sunaz)
+                #dshit = rotateline(dshit3.geometry.get_coordinates(),wallaz, sunaz)
 
-
-        if sunaz < 180:
-            compaz=sunaz+180
-        else:
-            compaz=sunaz-180
-
-        tandeg=np.rad2deg(np.deg2rad(elevation))
-
-        slength=  height / tandeg
-
-        #Deb("line length ")
-        #Deb(dshit4.geometry.length)
-        #Deb("line x ")
-        #Deb(dshit4.geometry)
+                dshit4 = gpd.GeoDataFrame(geometry=[dshitline], crs="WGS84")
+                dshit2=dshit4
+                dshit=dshit4
 
 
 
-
-        #############################################
-        #############################################
-        #####now call makerec2 to make shadow box####
-        #############################################
-        #############################################
-
-
-
-        deglengthlat=slength * (1/111111)
-        deglengthlon=slength * (1/(111111*math.cos(40)))
-        deglength=8*3*math.fabs((deglengthlat+deglengthlon))/2
-
-        Deb(f"Shadow len {slength:.2f} -> {deglength:.7f}   {(deglength*111111):.2f}  m")
-
-
-
-
-
-
-        for k in range(0,1):
-#            newshadow = makerecdeg(dshit, deglength, k)
-            newshadow = makerec3(b, slength, k)
-
-            Deb(type(newshadow))
-
-            barney = newshadow.to_json(to_wgs84=True)  # =True)  #  crs="EPSG:3627"
-
-
-
-
+            barney = dshit4.to_json(to_wgs84=True)  # =True)  #  crs="EPSG:3627"
 
             with open("/tmp/fromwall" + str(wallnum) + ".json", "w") as w1:
                 w1.write(barney)
                 w1.close()
 
 
-            if wallnum==1:
-                total=newshadow
-            else:
-                gdfunion=gpd.overlay(total,newshadow,how='union',keep_geom_type=False)
-                total=gdfunion
+    #        if sunaz < 180:
+    #            compaz=sunaz+180
+    #        else:
+    #            compaz=sunaz-180
 
-    total=total.sjoin(total,how="inner")
-    return total
+            tandeg=np.rad2deg(np.deg2rad(elevation))
+
+            slength=  height / tandeg
+
+            #Deb("line length ")
+            #Deb(dshit4.geometry.length)
+            #Deb("line x ")
+            #Deb(dshit4.geometry)
 
 
-def main():
+
+
+            #############################################
+            #############################################
+            #####now call makerec2 to make shadow box####
+            #############################################
+            #############################################
+
+
+
+            deglengthlat=slength * (1/111111)
+            deglengthlon=slength * (1/(111111*math.cos(40)))
+            deglength=8*3*math.fabs((deglengthlat+deglengthlon))/2
+
+            Deb(f"Shadow len {slength:.2f} -> {deglength:.7f}   {(deglength*111111):.2f}  m")
+
+
+
+
+
+
+            for k in range(0,1):
+    #            newshadow = makerecdeg(dshit4, deglength, k)
+                newshadow = makerec3(dshitline, 7*slength, k) # was b
+
+                Deb(type(newshadow))
+
+                barney = newshadow.to_json(to_wgs84=True)  # =True)  #  crs="EPSG:3627"
+
+
+
+
+
+                with open("/tmp/bigwall" + str(wallnum) + ".json", "w") as w1:
+                    w1.write(barney)
+                    w1.close()
+
+
+                if wallnum==1:
+                    total=newshadow
+                else:
+                    gdfunion=gpd.overlay(total,newshadow,how='union',keep_geom_type=False)
+                    total=gdfunion
+
+        total=total.sjoin(total,how="inner")
+        return total
+
+
+
+
+#############################################
+
+
+def wall2polygon(wall_list):
+    """!
+@callergraph
+
+
+
+@callgraph
+    """
+
 
     global total
 
 
-    dlist = [(-73.971558876777, 40.756579563884), (-73.971583976616, 40.756537332775), (-73.97153870727, 40.75651820027), (-73.971458333347, 40.756484232235), (-73.971510010991, 40.75641352834), (-73.971514021216, 40.756415223183), (-73.971564459855, 40.756436539799), (-73.971612762728, 40.756370451916), (-73.971558313901, 40.75634744048), (-73.971580059362, 40.756317688647), (-73.97160519838, 40.756283293739), (-73.971755861794, 40.756346968589), (-73.971966606825, 40.756436035918), (-73.971917137126, 40.75650372026), (-73.971842763583, 40.756472287977), (-73.971820192309, 40.756503168892), (-73.971808961763, 40.756518534168), (-73.971834435727, 40.756529299637), (-73.971858272213, 40.756539374012), (-73.971847763277, 40.756553752515), (-73.971817644113, 40.75659496128), (-73.971647241465, 40.756559142392), (-73.97161694632, 40.75659895798), (-73.971591643035, 40.756593410934)]
 
-    dshit8=wall2polygon(dlist)   
 
+
+#############################################
+
+
+
+
+# Function to extract wall segments as (long, lat) tuples
+def extract_wall_coords(geometry):
+    Deb('extract_wall_coords')
+    Deb(geometry.geom_type)
+    walls = []
+    if geometry.geom_type == 'LineString':
+        coords = list(geometry.coords)
+        for i in range(len(coords) - 1):
+            walls.append((coords[i], coords[i+1]))
+    elif geometry.geom_type == 'MultiLineString':
+        for line in geometry.geoms:
+            coords = list(line.coords)
+            for i in range(len(coords) - 1):
+                walls.append((coords[i], coords[i+1]))
+    return walls
+
+
+def unwraplist(alist):
+    for wall1 in alist:
+        for wall2 in wall1:
+            Deb(wall2)
+
+
+def main():
+    """!
+@callergraph
+
+
+
+@callgraph
+    """
+
+    global total
+
+
+    dlist=[]
+
+#############################################
+#############################################
+###read in CSV###############################
+#############################################
+#############################################
+
+    df = []
+    df = pd.read_csv(
+        "/Src/sun/just1.csv", sep=",")    
+
+    df['geometry'] = df['geometry'].apply(loads)
+
+    pd.set_option('display.max_colwidth',None)
+    pd.set_option('display.max_columns',None)
+    
+    pd.set_option('display.max_rows',None)
+    dshit12 = gpd.GeoDataFrame(df, crs="WGS84")
+    dshit12['walls'] = dshit12.geometry.boundary.apply(extract_wall_coords)
+
+    print(dshit12[['NAME', 'walls']].iloc[0])
+#    print(dshit12)
+
+    Deb(type(dshit12[['walls']].iloc[0])   )
+    prepredlist=dshit12[['walls']]
+    Deb(type(prepredlist))
+    Deb(type(prepredlist.iloc[0]))
+    predlist=pd.Series(prepredlist.iloc[0])
+    dlist=predlist.to_list()
+    Deb(dlist)
+
+
+    dshit8=wall2polygon2(dlist)   
+#    dshit8=unwraplist(dlist)   
+
+
+    
     Deb(type(dshit8))
 
-    if not (dshit8.is_valid).all():
-        Deb("dshit is not valid")
+#    if not (dshit8.is_valid).all():
+#        Deb("dshit is not valid")
 
 
     dshit8=dshit8.union_all()
@@ -241,8 +307,8 @@ def main():
     dshit9.set_crs("EPSG:2263",inplace=True)
     dshit8=dshit9
     dshit8['centroid']=dshit8['geometry'].centroid
-    print(type(dshit8['centroid']))
-    print(dshit8['centroid'])
+    Deb(type(dshit8['centroid']))
+    Deb(dshit8['centroid'])
 
 
 main()
@@ -328,4 +394,12 @@ A wall has a height of h.   The sun shines at a certain azimuth and elevation.  
 
 from a shapely linestring I want to create a rectangle by duplicating the linestring 3 meters away and then creating lines to complete the rectangle.  The initial linestring is defined in long/lat and all points need to be in long/lat when the rectangle is defined.  The rectangle will then be changed into a polygon in geopandas.  previous example code resulted in "AttributeError: module 'shapely.geometry' has no attribute 'line_merge'"
 
+'''
+
+'''
+    dlist=[(-73.971815685417,40.756642525492), (-73.971845414769,40.756601849732), (-73.971882883991,40.756617685264), (-73.971892228211,40.756621634459), (-73.97194539848,40.756548887488), (-73.971898585074,40.756529102783), (-73.971917137126,40.75650372026), (-73.971966606825,40.756436035918), (-73.972187677007,40.756529466501), (-73.972014616104,40.756766249388), (-73.971949620799,40.756738780776), (-73.971793544195,40.756672818476), (-73.971815685417,40.756642525492)]
+
+##135 e 50    dlist = [(-73.971558876777, 40.756579563884), (-73.971583976616, 40.756537332775), (-73.97153870727, 40.75651820027), (-73.971458333347, 40.756484232235), (-73.971510010991, 40.75641352834), (-73.971514021216, 40.756415223183), (-73.971564459855, 40.756436539799), (-73.971612762728, 40.756370451916), (-73.971558313901, 40.75634744048), (-73.971580059362, 40.756317688647), (-73.97160519838, 40.756283293739), (-73.971755861794, 40.756346968589), (-73.971966606825, 40.756436035918), (-73.971917137126, 40.75650372026), (-73.971842763583, 40.756472287977), (-73.971820192309, 40.756503168892), (-73.971808961763, 40.756518534168), (-73.971834435727, 40.756529299637), (-73.971858272213, 40.756539374012), (-73.971847763277, 40.756553752515), (-73.971817644113, 40.75659496128), (-73.971647241465, 40.756559142392), (-73.97161694632, 40.75659895798), (-73.971591643035, 40.756593410934)]
+
+##    dlist = [(-73.946942654932 ,40.806222154761), (-73.946977239811, 40.80617461534), (-73.947156723315, 40.806250005546), ( -73.947138469349, 40.806275095875), (-73.947122138526, 40.806297544119),( -73.947066845689, 40.8062743192),( -73.946942654932, 40.806222154761)]
 '''
