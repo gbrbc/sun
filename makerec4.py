2ef8325a4dc9ccd362fa560b2e05520ae9f8e4c9
gbrbc <brendanreillybc@gmail.com>
2025-07-22 19:20:32 -0400

    fix, should not use 2263

--- makerec4.py
+++ makerec4.py
@@ -1,29 +1,65 @@
 import geopandas as gpd
 from shapely.geometry import LineString, Polygon
-from shapely.ops import transform
+# rec3  from shapely.ops import transform
+from pyproj import Transformer
 from functools import partial
-import pyproj
+from shapely.ops import transform
 
 from geotools import *
 
 
+def Deb(msg=""):
+    """!
+@callergraph
 
-def makerec3(line_coords,buffer_distance,k):
+
+
+@callgraph
+    """
+
+    print(f"DebugMR {sys._getframe().f_back.f_lineno}: {msg}", flush=True,file=sys.stderr)
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+
+
+
+
+def makerec4(line_coords,buffer_distance,k):
+
+## FIX wgx84/2263     32618
+## transformer2m = Transformer.from_crs("EPSG:2263", "EPSG:32618")
+## transformer2lat = Transformer.from_crs( "EPSG:32618","EPSG:2263")
+
+## transformer2m(2,3)
+## transformer2lat(3,2)
 
     Deb("STARTED MAKEREC4")
+
     # 1. Define the initial LineString in longitude/latitude
-    #line_coords = [(-74.006, 40.7128), (-74.005, 40.7130)]
-#    line_coords = [(-73.971558876777, 40.756579563884), (-73.971583976616, 40.756537332775)]
+
 
 
     original_line = LineString(line_coords)
 
     # 2. Define the buffer distance in meters
-#    buffer_distance = 3
+    #    buffer_distance = 3
 
     # 3. Reproject to a local Azimuthal Equidistant (aeqd) projection for accurate buffering in meters
     #    Center the projection on the midpoint of the line for best accuracy.
+
+
+
+
+###NEED TO LOOK AT DIRECTIONS MORE CAREFULLY
+##https://pyproj4.github.io/pyproj/stable/api/transformer.html    
+
+    transformer2m = Transformer.from_crs("EPSG:2263", "EPSG:32618", always_xy=True)
+    transformer2lat = Transformer.from_crs( "EPSG:32618","EPSG:2263", always_xy=True)
+
     centroid = original_line.centroid
+
+    """
     local_aeqd = f"+proj=aeqd +R=6371000 +units=m +lat_0={centroid.y} +lon_0={centroid.x}"
     project_to_aeqd = partial(
         pyproj.transform,
@@ -35,22 +71,39 @@ def makerec3(line_coords,buffer_distance,k):
         pyproj.Proj(local_aeqd),      # Source CRS (local aeqd)
         pyproj.Proj(init='epsg:4326')  # Target CRS (WGS84)
     )
+    """
 
-    projected_line = transform(project_to_aeqd, original_line)
+
+#    projected_line = transform(project_to_aeqd, original_line)
+
+    projected_line=transform(transformer2m.transform,original_line)
+
+########lands a line 1/2 round globe, using wrong epsg
+
+    if k==10:
+        parallel_line_mitre = original_line.parallel_offset(buffer_distance, 'mitre')
+        gdfp = gpd.GeoDataFrame(geometry=[parallel_line_mitre], crs='wgs84')
+        bname = "/tmp/wilma10.json"
+        pebbles = gdfp.to_json(to_wgs84=True)  # True)  #  crs="WGS84"
+        with open(bname, "w") as w:
+            w.write(pebbles)
+            w.close()
+
+
 
     # 4. Create the buffer (rectangle) in the projected CRS
     buffered_shape = projected_line.buffer(buffer_distance, cap_style=2) # cap_style=2 for flat ends
 
     # 5. Reproject the buffered shape back to longitude/latitude (WGS84)
-    final_polygon = transform(project_to_wgs84, buffered_shape)
-
+#    final_polygon = transform(project_to_wgs84, buffered_shape)
+    final_polygon = transform(transformer2lat.transform,buffered_shape)
     # 6. Create a GeoDataFrame from the polygon
     gdf = gpd.GeoDataFrame(geometry=[final_polygon], crs='epsg:4326')
 
 #    print(f"Original LineString: {original_line}")
 #    print(f"Resulting Polygon (GeoDataFrame): {gdf}")
 
-    fake=17
+    fake=k
     bname = "/tmp/bldg" + str(fake) + ".json"
     pebbles = gdf.to_json(to_wgs84=True)  # True)  #  crs="WGS84"
     with open(bname, "w") as w:
