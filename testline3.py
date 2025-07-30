f64e439951c02a9d0a5cb8fc22cbb3d77571c6eb
gbrbc <brendanreillybc@gmail.com>
2025-07-30 19:06:15 -0400

    It's Alive (and working)

--- testline3.py
+++ testline3.py
@@ -1,11 +1,12 @@
-
+#!/opt/local/bin/python3 
 
 import warnings
 warnings.filterwarnings("error", category=UserWarning)
 import sys
 import os
 import shapely
-from shapely.geometry import Polygon, MultiPolygon, Point, LineString
+from shapely.geometry import Polygon, MultiPolygon,  LineString
+##removed Point
 from shapely.affinity import translate
 from shapely.wkt import loads
 import pyproj
@@ -19,6 +20,7 @@ import geopy.distance
 import random
 
 import geographiclib
+from geopy.point import Point
 
 from rotateline import *
 
@@ -92,12 +94,57 @@ line19 = LineString([( -73.972060000,40.756452513),(-73.972060000, 40.756407487)
 
 #bylist=[(Point( -73.97206,40.75645251254337, 0.0), Point( -73.97206, 40.75640748745654, 0.0)), (Point(40.75645115487044, -73.97204987436471, 0.0), Point(40.75640884512859, -73.97207012562886, 0.0)), (Point(40.75644724560719, -73.9720409700316, 0.0), Point(40.75641275438962, -73.97207902995856, 0.0)), (Point(40.75644125626885, -73.97203436099522, 0.0), Point(40.75641874372543, -73.97208563899612, 0.0)), (Point(40.756433909258455, -73.97203084440274, 0.0), Point(40.75642609073418, -73.97208915559385, 0.0)), (Point(40.75642609073418, -73.97203084440615, 0.0), Point(40.756433909258455, -73.97208915559726, 0.0)), (Point(40.75641874372543, -73.97203436100388, 0.0), Point(40.75644125626885, -73.97208563900477, 0.0)), (Point(40.75641275438962, -73.97204097004143, 0.0), Point(40.75644724560719, -73.9720790299684, 0.0)), (Point(40.75640884512859, -73.97204987437114, 0.0), Point(40.75645115487044, -73.97207012563528, 0.0)), (Point(40.75640748745654, -73.97206, 0.0), Point(40.75645251254337, -73.97206, 0.0)), (Point(40.75640884512859, -73.97207012562886, 0.0), Point(40.75645115487044, -73.97204987436471, 0.0)), (Point(40.75641275438962, -73.97207902995856, 0.0), Point(40.75644724560719, -73.9720409700316, 0.0)), (Point(40.75641874372543, -73.97208563899612, 0.0), Point(40.75644125626885, -73.97203436099522, 0.0)), (Point(40.75642609073418, -73.97208915559385, 0.0), Point(40.756433909258455, -73.97203084440274, 0.0)), (Point(40.756433909258455, -73.97208915559726, 0.0), Point(40.75642609073418, -73.97203084440615, 0.0)), (Point(40.75644125626885, -73.97208563900477, 0.0), Point(40.75641874372543, -73.97203436100388, 0.0)), (Point(40.75644724560719, -73.9720790299684, 0.0), Point(40.75641275438962, -73.97204097004143, 0.0)), (Point(40.75645115487044, -73.97207012563528, 0.0), Point(40.75640884512859, -73.97204987437114, 0.0)), (Point(40.75645251254337, -73.97206, 0.0), Point(40.75640748745654, -73.97206, 0.0))]
 
-bylist=[(Point( -73.97206,40.75645251254337, 0.0), Point( -73.97206, 40.75640748745654, 0.0)),(Point(-73.97204987436471, 40.75645115487044, 0.0), Point(-73.97207012562886, 40.75640884512859, 0.0)), (Point(-73.9720409700316, 40.75644724560719, 0.0), Point(-73.97207902995856, 40.75641275438962, 0.0)), (Point(-73.97203436099522, 40.75644125626885, 0.0), Point(-73.97208563899612, 40.75641874372543, 0.0)), (Point(-73.97203084440274, 40.756433909258455, 0.0), Point(-73.97208915559385, 40.75642609073418, 0.0)), (Point(-73.97203084440615, 40.75642609073418, 0.0), Point(-73.97208915559726, 40.756433909258455, 0.0)), (Point(-73.97203436100388, 40.75641874372543, 0.0), Point(-73.97208563900477, 40.75644125626885, 0.0)), (Point(-73.97204097004143, 40.75641275438962, 0.0), Point(-73.9720790299684, 40.75644724560719, 0.0)), (Point(-73.97204987437114, 40.75640884512859, 0.0), Point(-73.97207012563528, 40.75645115487044, 0.0)), (Point(-73.97206, 40.75640748745654, 0.0), Point(-73.97206, 40.75645251254337, 0.0)), (Point(-73.97207012562886, 40.75640884512859, 0.0), Point(-73.97204987436471, 40.75645115487044, 0.0)), (Point(-73.97207902995856, 40.75641275438962, 0.0), Point(-73.9720409700316, 40.75644724560719, 0.0)), (Point(-73.97208563899612, 40.75641874372543, 0.0), Point(-73.97203436099522, 40.75644125626885, 0.0)), (Point(-73.97208915559385, 40.75642609073418, 0.0), Point(-73.97203084440274, 40.756433909258455, 0.0)), (Point(-73.97208915559726, 40.756433909258455, 0.0), Point(-73.97203084440615, 40.75642609073418, 0.0)), (Point(-73.97208563900477, 40.75644125626885, 0.0), Point(-73.97203436100388, 40.75641874372543, 0.0)), (Point(-73.9720790299684, 40.75644724560719, 0.0), Point(-73.97204097004143, 40.75641275438962, 0.0)), (Point(-73.97207012563528, 40.75645115487044, 0.0), Point(-73.97204987437114, 40.75640884512859, 0.0)), (Point(-73.97206, 40.75640748745654, 0.0), Point(-73.97206, 40.75645251254337, 0.0))]
+#29Jul  bylist=[(Point( -73.97206,40.75645251254337, 0.0), Point( -73.97206, 40.75640748745654, 0.0)),(Point(-73.97204987436471, 40.75645115487044, 0.0), Point(-73.97207012562886, 40.75640884512859, 0.0)), (Point(-73.9720409700316, 40.75644724560719, 0.0), Point(-73.97207902995856, 40.75641275438962, 0.0)), (Point(-73.97203436099522, 40.75644125626885, 0.0), Point(-73.97208563899612, 40.75641874372543, 0.0)), (Point(-73.97203084440274, 40.756433909258455, 0.0), Point(-73.97208915559385, 40.75642609073418, 0.0)), (Point(-73.97203084440615, 40.75642609073418, 0.0), Point(-73.97208915559726, 40.756433909258455, 0.0)), (Point(-73.97203436100388, 40.75641874372543, 0.0), Point(-73.97208563900477, 40.75644125626885, 0.0)), (Point(-73.97204097004143, 40.75641275438962, 0.0), Point(-73.9720790299684, 40.75644724560719, 0.0)), (Point(-73.97204987437114, 40.75640884512859, 0.0), Point(-73.97207012563528, 40.75645115487044, 0.0)), (Point(-73.97206, 40.75640748745654, 0.0), Point(-73.97206, 40.75645251254337, 0.0)), (Point(-73.97207012562886, 40.75640884512859, 0.0), Point(-73.97204987436471, 40.75645115487044, 0.0)), (Point(-73.97207902995856, 40.75641275438962, 0.0), Point(-73.9720409700316, 40.75644724560719, 0.0)), (Point(-73.97208563899612, 40.75641874372543, 0.0), Point(-73.97203436099522, 40.75644125626885, 0.0)), (Point(-73.97208915559385, 40.75642609073418, 0.0), Point(-73.97203084440274, 40.756433909258455, 0.0)), (Point(-73.97208915559726, 40.756433909258455, 0.0), Point(-73.97203084440615, 40.75642609073418, 0.0)), (Point(-73.97208563900477, 40.75644125626885, 0.0), Point(-73.97203436100388, 40.75641874372543, 0.0)), (Point(-73.9720790299684, 40.75644724560719, 0.0), Point(-73.97204097004143, 40.75641275438962, 0.0)), (Point(-73.97207012563528, 40.75645115487044, 0.0), Point(-73.97204987437114, 40.75640884512859, 0.0)), (Point(-73.97206, 40.75640748745654, 0.0), Point(-73.97206, 40.75645251254337, 0.0))]
 
+
+#bylist=[( Point(-73.972060000, 40.756407487),Point( -73.972060000,40.756452513)),(Point( -73.972049874,40.756451155), Point(-73.972070126, 40.756408845)),(Point( -73.972040970,40.756447246), Point(-73.972079030, 40.756412754)),(Point( -73.972034361,40.756441256), Point(-73.972085639, 40.756418744)),(Point( -73.972030844,40.756433909), Point(-73.972089156, 40.756426091)),(Point( -73.972030844,40.756426091), Point(-73.972089156, 40.756433909)),(Point( -73.972034361,40.756418744), Point(-73.972085639, 40.756441256)),(Point( -73.972040970,40.756412754), Point(-73.972079030, 40.756447246)),(Point( -73.972049874,40.756408845), Point(-73.972070126, 40.756451155)),(Point( -73.972060000,40.756407487), Point(-73.972060000, 40.756452513)),(Point( -73.972070126,40.756408845), Point(-73.972049874, 40.756451155)),(Point( -73.972079030,40.756412754), Point(-73.972040970, 40.756447246)),(Point( -73.972085639,40.756418744), Point(-73.972034361, 40.756441256)),(Point( -73.972089156,40.756426091), Point(-73.972030844, 40.756433909)),(Point( -73.972089156,40.756433909), Point(-73.972030844, 40.756426091)),(Point( -73.972085639,40.756441256), Point(-73.972034361, 40.756418744)),(Point( -73.972079030,40.756447246), Point(-73.972040970, 40.756412754)),(Point( -73.972070126,40.756451155), Point(-73.972049874, 40.756408845)),(Point( -73.972060000,40.756452513), Point(-73.972060000, 40.756407487))]
+
+bylist=[(Point(40.75645251254337, -73.97206, 0.0), Point(40.75640748745654, -73.97206, 0.0)), (Point(40.75645115487044, -73.97204987436471, 0.0), Point(40.75640884512859, -73.97207012562886, 0.0)), (Point(40.75644724560719, -73.9720409700316, 0.0), Point(40.75641275438962, -73.97207902995856, 0.0)), (Point(40.75644125626885, -73.97203436099522, 0.0), Point(40.75641874372543, -73.97208563899612, 0.0)), (Point(40.756433909258455, -73.97203084440274, 0.0), Point(40.75642609073418, -73.97208915559385, 0.0)), (Point(40.75642609073418, -73.97203084440615, 0.0), Point(40.756433909258455, -73.97208915559726, 0.0)), (Point(40.75641874372543, -73.97203436100388, 0.0), Point(40.75644125626885, -73.97208563900477, 0.0)), (Point(40.75641275438962, -73.97204097004143, 0.0), Point(40.75644724560719, -73.9720790299684, 0.0)), (Point(40.75640884512859, -73.97204987437114, 0.0), Point(40.75645115487044, -73.97207012563528, 0.0)), (Point(40.75640748745654, -73.97206, 0.0), Point(40.75645251254337, -73.97206, 0.0)), (Point(40.75640884512859, -73.97207012562886, 0.0), Point(40.75645115487044, -73.97204987436471, 0.0)), (Point(40.75641275438962, -73.97207902995856, 0.0), Point(40.75644724560719, -73.9720409700316, 0.0)), (Point(40.75641874372543, -73.97208563899612, 0.0), Point(40.75644125626885, -73.97203436099522, 0.0)), (Point(40.75642609073418, -73.97208915559385, 0.0), Point(40.756433909258455, -73.97203084440274, 0.0)), (Point(40.756433909258455, -73.97208915559726, 0.0), Point(40.75642609073418, -73.97203084440615, 0.0)), (Point(40.75644125626885, -73.97208563900477, 0.0), Point(40.75641874372543, -73.97203436100388, 0.0)), (Point(40.75644724560719, -73.9720790299684, 0.0), Point(40.75641275438962, -73.97204097004143, 0.0)), (Point(40.75645115487044, -73.97207012563528, 0.0), Point(40.75640884512859, -73.97204987437114, 0.0)), (Point(40.75645251254337, -73.97206, 0.0), Point(40.75640748745654, -73.97206, 0.0))]
 
 
 
 listlin=[line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14, line15, line16, line17, line18, line19]
+
+
+def calculate_azimuth(ax, ay, bx, by):
+    """Computes the bearing in degrees from the point A(ax,ay) to the point B(bx,by)."""
+    a=Geodesic.WGS84.Inverse(ay,  ax,  by,  bx, outmask=1929)
+    prelim=a['azi1']
+    if prelim==360 or prelim==180:
+        return 0
+    if prelim < 0:
+        prelim=360+prelim
+    if prelim >= 180:
+        prelim=prelim-180
+    if prelim==360 or prelim==180:
+        return 0
+
+    return prelim
+
+    TWO_PI = math.pi * 2
+    theta = math.atan2(bx - ax, ay - by)
+    if theta < 0.0:
+        theta += TWO_PI
+    return math.degrees(theta)
+
+
+
+
+def calculate_azimuth_line2(aline):
+    if not isinstance(aline,LineString):
+        raise  TypeError("supply LineString instead")
+    first_point = Point(aline.coords[0])
+   
+    last_point = Point(aline.coords[-1]) # Or line.coords[1] for a simple two-point line
+
+    # Calculate azimuth
+#    return calculate_azimuth(first_point.longitude, first_point.latitude, last_point.longitude, last_point.latitude)
+    return calculate_azimuth(first_point.longitude, first_point.latitude, last_point.longitude, last_point.latitude)
+
+
+
+
+
 
 gpd.options.display_precision = 9
 bearing=0
@@ -110,38 +157,94 @@ for i, (p1, p2) in enumerate(bylist):
 
     print ( '#' * 45)
 
+#    Deb(f"STEP2  {p1}")
+#    print(f"line{ls} = LineString([( {p1.longitude:.9f},{p1.latitude:.9f}),({p2.longitude:.9f}, {p2.latitude:.9f})])")
 
-    print(f"line{ls} = LineString([( {p1.longitude:.9f},{p1.latitude:.9f}),({p2.longitude:.9f}, {p2.latitude:.9f})])")
-
+    '''
     if ls==2:
         print(p1)
         print(p2)
         Deb(howlong(p1,p2))
 
+    '''
 
-    line = LineString([p1, p2])
 
-    Deb(line)
+    cline = LineString([p1, p2])
+    centroid = cline.centroid
+    Deb(f"cline  {cline}")
+
+###cline is a fake just to get the centroid to build real line
+
+    abclen=geopy.distance.geodesic(p1, p2).meters
+    Deb(f"  len  {abclen:.2f}")
+
+
+###Do here what rotate was supposed to
+
+
+    Deb('test 1')
+    centroid=Point(centroid.x,centroid.y)
+    Deb(f"centroid  {centroid}")
+    Deb(f"args   {abclen/2000}   {centroid}  {bearing}")
+
+
     
-    qaorig=calculate_azimuth_line(line)
+#    point1 = geodesic(kilometers=abclen/2000).destination(point=centroid, bearing=bearing)
+    point1 = geodesic(meters=2.5).destination(point=centroid, bearing=bearing)
+    Deb(f"point1 post-geodesic STEP2  {point1}")
+    Deb(f"point1  {type(point1)}")    
+
+    opposite_bearing = (bearing + 180) #% 360
+    point2 = geodesic(kilometers=abclen/2000).destination(point=centroid, bearing=opposite_bearing)
+    Deb(f"point2 post-geodesic STEP2a {point2}")
+    Deb(f"point2  {type(point2)}")    
+##blameme   points have equal value, but linestring has diff seconds for long
+##blame the lines are 5meters apart in southern direction
+    line=LineString([point1, point2])
+    Deb(f"centroid  {centroid}")
+    Deb(f"line post-geodesic  {line}  bearing {bearing}  opp  {opposite_bearing}")
+    Deb(f"STEP3 line  {type(line)}")
+
+    Deb(f"newline {line}")
+    qaorig=calculate_azimuth_line2(line)
 
     Deb(f"#manufactured bearing {bearing} vs actual {qaorig:.1f}")
 
     if False==closeaz(bearing,qaorig):
-        raise ValueError("bearing diff qaorig >5")
+        Deb('bearing off')
+#        raise ValueError("bearing diff qaorig >5")
 
 
-
+    '''
     randomcompass=random.randint(0,359)
 
     dshitline = rotateline(LineString(line), randomcompass)
 
     qa=calculate_azimuth_line(dshitline)
-#    if False==closeaz(133,qa):
-#        raise ValueError("bearing diff qa >5")
 
-    if False==closeaz(randomcompass,qa):
-        raise ValueError(str(randomcompass)+" diff qa " + str(qa) + ">5")
+    Deb(dshitline)
+
+    '''
+##took out dshitline,
+##30jul    dshit4 = gpd.GeoDataFrame(geometry=[line], crs="WGS84")    
+    dshit4 = gpd.GeoDataFrame(geometry=[LineString([(point1.longitude,point1.latitude),(point2.longitude,point2.latitude)])], crs="WGS84")    
+    fname = f"/tmp/ts{bearing:d}Q{qaorig:.0f}.json"
+    barney = dshit4.to_file(fname,driver="GeoJSON")  # =True)  #  crs="EPSG:3627"
+
+
+    dshit4 = gpd.GeoDataFrame(geometry=[line.centroid], crs="WGS84")    
+
+    fname = f"/tmp/ts{bearing:d}Q{qaorig:.0f}CENT.json"
+#    barney = dshit4.to_file(fname,driver="GeoJSON")  # =True)  #  crs="EPSG:3627"
+
+
+
+
+
+    
+
+#    if False==closeaz(bearing,qa):
+#        raise ValueError(str(bearing)+" diff qa " + str(qa) + ">5")
 
 
 
@@ -152,6 +255,30 @@ for i, (p1, p2) in enumerate(bylist):
 
         
 exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #p1=( -73.972060000,40.756452513)
 #p2=(-73.972060000, 40.756407487)
