9b6de642351565acf61cfd3560ed73e48a868e89
gbrbc <brendanreillybc@gmail.com>
2025-08-01 13:37:22 -0400

    still cannot make rectangle

--- makerec.py
+++ makerec.py
@@ -4,6 +4,8 @@ from shapely.geometry import LineString, Polygon
 from pyproj import Transformer
 from functools import partial
 from shapely.ops import transform
+from shapely.validation import make_valid,explain_validity
+import pyproj
 
 from geotools import *
 
@@ -59,11 +61,11 @@ def makerec(line_coords,buffer_distance,k):
 
 ###NEED TO LOOK AT DIRECTIONS MORE CAREFULLY
 ##https://pyproj4.github.io/pyproj/stable/api/transformer.html    
-
-    transformer2m = Transformer.from_crs(crs_from="EPSG:4326", crs_to="EPSG:32618", always_xy=True,only_best=True)
+    Deb('RecStep1')
+    transformer2m = pyproj.Transformer.from_crs(crs_from="EPSG:4326", crs_to="EPSG:32618", always_xy=True,only_best=True)
  # ,errcheck=True
-
-    transformer2lat = Transformer.from_crs( "EPSG:32618","EPSG:4326", always_xy=True,only_best=True)
+    Deb('RecStep2')
+    transformer2lat = pyproj.Transformer.from_crs( "EPSG:32618","EPSG:4326", always_xy=True,only_best=True)
  # ,errcheck=True
 
 
@@ -76,6 +78,9 @@ def makerec(line_coords,buffer_distance,k):
         pyproj.Proj(init='epsg:4326'), # Source CRS (WGS84)
         pyproj.Proj(local_aeqd)       # Target CRS (local aeqd)
     )
+
+    Deb('RecStep3')
+
     project_to_wgs84 = partial(
         pyproj.transform,
         pyproj.Proj(local_aeqd),      # Source CRS (local aeqd)
@@ -83,16 +88,31 @@ def makerec(line_coords,buffer_distance,k):
     )
     """
 
+    assert original_line.is_valid
+
+
 
 #    projected_line = transform(project_to_aeqd, original_line)
 
     projected_line=transform(transformer2m.transform,original_line)
 
+    Deb("projected_line")
+    Deb(projected_line)
+
+    assert projected_line.is_valid
+
+
+
+
+    Deb('RecStep4')
+
 ########lands a line 1/2 round globe, using wrong epsg
     """
-    if k==10:
+    if k==1:
         parallel_line_mitre = original_line.parallel_offset(buffer_distance, 'mitre')
         gdfp = gpd.GeoDataFrame(geometry=[parallel_line_mitre], crs='wgs84')
+        Deb('RecStep5')
+
         bname = "/tmp/wilma10.json"
         pebbles = gdfp.to_json(to_wgs84=True)  # True)  #  crs="WGS84"
         with open(bname, "w") as w:
@@ -102,17 +122,62 @@ def makerec(line_coords,buffer_distance,k):
 
 
     # 4. Create the buffer (rectangle) in the projected CRS
+    Deb('RecStep6')
+
+    assert buffer_distance>=2
+
+    writeWGS(original_line,"/tmp/bs1.json")
+
+    writeWGS(projected_line,"/tmp/bs2.json")
+
+
     buffered_shape = projected_line.buffer(buffer_distance, cap_style=2,single_sided=True) # cap_style=2 for flat ends
+
+    writeWGS(buffered_shape,"/tmp/bs3.json")
+
+    Deb('RecStep7')
 
     # 5. Reproject the buffered shape back to longitude/latitude (WGS84)
 #    final_polygon = transform(project_to_wgs84, buffered_shape)
+
+#    Deb(f"buffered_shape  {buffered_shape}  {type(buffered_shape)}")
+
+
+    Deb(explain_validity(buffered_shape))
+    assert buffered_shape.is_valid
+    
     final_polygon = transform(transformer2lat.transform,buffered_shape)
+
+
+    Deb(explain_validity(final_polygon))
+    assert final_polygon.is_valid
+
+
+
     # 6. Create a GeoDataFrame from the polygon
 
 ##    final_polygon['newwallnum']=k
 
+    Deb('RecStep8')
+#    gdf = gpd.GeoDataFrame(geometry=[final_polygon], crs='epsg:4326')
+    gdf = gpd.GeoDataFrame(geometry=[final_polygon], crs='wgs84')
 
-    gdf = gpd.GeoDataFrame(geometry=[final_polygon], crs='epsg:4326')
+    if not (gdf.is_valid).all():
+        Deb(explain_validity(gdf))
+        newgdf=make_valid(gdf)
+        gdf=newgdf
+
+    assert (gdf.is_valid).all()
+
+
+
+
+
+
+
+
+
+
 
 
 #    print(f"Original LineString: {original_line}")
